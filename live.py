e7135cda22ad18155d24c9a448981f08be4d7b63
aradnav <116241847+aradnav@users.noreply.github.com>
2024-01-22 13:25:12 +0700

    Add files via upload

---
+++ live.py
@@ -0,0 +1,45 @@
+#Importing Libraries
+import cv2
+import supervision as sv
+from ultralytics import YOLO
+
+def live_object_detection():
+    '''main Function'''
+    # Initialize the camera
+    cap = cv2.VideoCapture(0)
+    model = YOLO("model_- 22 january 2024 10_45.pt")
+
+    box_annotator = sv.BoxAnnotator(
+        thickness=2,
+        text_thickness=2,
+        text_scale=1
+    )
+
+    while True:
+        ret, frame = cap.read()
+
+        data = model(frame)
+        detection = sv.Detections.from_ultralytics(data[0])
+
+        label = [f"{model.model.names[ci]} {con:0.2f}"
+                 for _, _, con, ci, _ in detection]
+
+        if not ret:
+            print("Error: Failed to grab frame.")
+            break
+
+        frame = box_annotator.annotate(scene=frame, detections=detection, labels=label)
+
+        # Display the live camera feed with object detection annotations
+        cv2.imshow("Live Object Detection", frame)
+
+        # Exit the loop if the 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the camera and close the OpenCV window
+    cap.release()
+    cv2.destroyAllWindows()
+
+# Run the live object detection function
+live_object_detection()
